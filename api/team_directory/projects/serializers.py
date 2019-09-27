2cc23dc05ce5bc5bbeb7a5757fa43469ac14ee62
Ömer Faruk Abacı <omerfabaci@gmail.com>
2019-09-27 13:01:09 +0300

    Add members to team and project serializers

--- api/team_directory/projects/serializers.py
+++ api/team_directory/projects/serializers.py
@@ -10,6 +10,14 @@ class ProjectSerializer(serializers.ModelSerializer):
         fields = '__all__'
         model = Project
 
+    def to_representation(self, instance):
+        from members.serializers import SimpleMemberSerializer
+
+        data = super(ProjectSerializer, self).to_representation(instance)
+        data["members"] = SimpleMemberSerializer(instance.members, many=True, context=self.context).data
+
+        return data
+
 
 class TeamSerializer(serializers.ModelSerializer):
     members = serializers.PrimaryKeyRelatedField(read_only=True, many=True)
@@ -17,3 +25,11 @@ class TeamSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Team
+
+    def to_representation(self, instance):
+        from members.serializers import SimpleMemberSerializer
+
+        data = super(TeamSerializer, self).to_representation(instance)
+        data["members"] = SimpleMemberSerializer(instance.members, many=True, context=self.context).data
+
+        return data
