12726e01f0e5376f40e0d2280ca23f39fdc1dd1a
Ömer Faruk Abacı <omerfabaci@gmail.com>
2019-09-27 11:41:48 +0300

    Add Links to MemberSerializer

--- api/team_directory/members/serializers.py
+++ api/team_directory/members/serializers.py
@@ -5,6 +5,7 @@ from members.models import Member
 
 class MemberSerializer(serializers.ModelSerializer):
     projects = serializers.PrimaryKeyRelatedField(read_only=True, many=True)
+    links = serializers.PrimaryKeyRelatedField(read_only=True, many=True)
 
     class Meta:
         fields = '__all__'
